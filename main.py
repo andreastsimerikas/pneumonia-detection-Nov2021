8ad47c9c4309065a94ddc39a1dd67917289ae24a
andreas tsimerikas <andreastsimerikas@gmail.com>
2021-11-18 16:43:42 +0200

    basic model

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import VGG16
+import os
 
-batch= 25
+batch= 10
 
 traindf = pd.read_csv('pneumonia/labels_train.csv', dtype=str)
 datagen = ImageDataGenerator(rescale=1./255.,validation_split=0.25)
 test_datagen = ImageDataGenerator(rescale=1./255.)
 
 train_path = 'pneumonia/train_images'
-test_path = 'pneumonia/test_images'
+test_path = 'pneumonia/test_images/images'
 
 train_batches = datagen.flow_from_dataframe(
     dataframe=traindf,
@@ -45,8 +46,18 @@ valid_batches = datagen.flow_from_dataframe(
     target_size=(224,224)
 )
 
-test_batches = test_datagen.flow_from_directory(
+test_images = []
+i=1
+for img in os.listdir(test_path):
+    test_images.append(img)
+    i+=1
+   
+submision = pd.DataFrame(test_images,columns=['file_name'])
+
+test_batches = test_datagen.flow_from_dataframe(
+    dataframe=submision,
     directory=test_path,
+    x_col="file_name",
     batch_size=1,
     class_mode=None,
     shuffle=False,
@@ -54,31 +65,8 @@ test_batches = test_datagen.flow_from_directory(
     target_size=(224, 224)
 )
 
-plt.figure(figsize=(12, 12))
-for i in range(0, 25):
-    plt.subplot(5, 5, i+1)
-    for images, labels in train_batches:
-        image = images[1]        
-        plt.title(labels[1])
-        plt.axis('off')
-        plt.imshow(np.squeeze(image),cmap='gray',interpolation='nearest')
-        break
-plt.tight_layout()
-plt.show()
-
 def create_model(input_shape, n_classes, optimizer='rmsprop', fine_tune=0):
-    """
-    Compiles a model integrated with VGG16 pretrained layers
-    
-    input_shape: tuple - the shape of input images (width, height, channels)
-    n_classes: int - number of classes for the output layer
-    optimizer: string - instantiated optimizer to use for training. Defaults to 'RMSProp'
-    fine_tune: int - The number of pre-trained layers to unfreeze.
-                If set to 0, all pretrained layers will freeze during training
-    """
-    
-    # Pretrained convolutional layers are loaded using the Imagenet weights.
-    # Include_top is set to False, in order to exclude the model's fully-connected layers.
+  
     conv_base = VGG16(include_top=False,
                         weights='imagenet', 
                         input_shape=input_shape)
@@ -93,8 +81,6 @@ def create_model(input_shape, n_classes, optimizer='rmsprop', fine_tune=0):
         for layer in conv_base.layers:
             layer.trainable = False
 
-    # Create a new 'top' of the model (i.e. fully-connected layers).
-    # This is 'bootstrapping' a new top_model onto the pretrained layers.
     top_model = conv_base.output
     top_model = Flatten(name="flatten")(top_model)
     top_model = Dense(4096, activation='relu')(top_model)
@@ -112,15 +98,14 @@ def create_model(input_shape, n_classes, optimizer='rmsprop', fine_tune=0):
     
     return model
 
-input_shape = (224, 224, 3)
-optim_1 = Adam(learning_rate=0.001)
-n_classes=3
+input_shape= (224, 224, 3)
+optim_1= Adam(learning_rate=0.001)
+n_classes= 3
 
-n_steps = train_batches.samples // batch
-n_val_steps = valid_batches.samples // batch
-n_epochs = 1
+n_steps= train_batches.samples // batch
+n_val_steps= valid_batches.samples // batch
+n_epochs= 1
 
-# First we'll train the model without Fine-tuning
 vgg_model = create_model(input_shape, n_classes, optim_1, fine_tune=0)
 
 vgg_history = vgg_model.fit(train_batches,
@@ -130,9 +115,19 @@ vgg_history = vgg_model.fit(train_batches,
                     validation_steps=n_val_steps,
                     verbose=1)   
 
-pred = vgg_model.predict(test_batches)
 
-pred_dt = pd.DataFrame(pred)
-print(pred_dt)
+plt.plot(vgg_history.history['acc'])
+plt.plot(vgg_history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()  
 
+predictions = vgg_model.predict(valid_batches,verbose=1)
+
+preds = np.argmax(predictions, axis=-1) 
+
+submision['class_id'] = preds
+submision.to_csv('submision.csv',index=False)
 
